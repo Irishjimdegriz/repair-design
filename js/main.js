89b75f1362e878a16cdfc2acbf2b2401cb3c5f26
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-05-13 19:17:02 +0300

    feat: modified closing rules for modal window

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+//прикручено к сайту
+
 document.addEventListener('DOMContentLoaded', function(event) {
 const modal = document.querySelector('.modal');
+const modalDialog = document.querySelector('.modal__dialog');
 const modalBtn = document.querySelectorAll('[data-toggle=modal]');
 const closeBtn = document.querySelector('.modal__close');
 
@@ -12,4 +15,23 @@ const switchModal = () => {
   });
 
   closeBtn.addEventListener('click', switchModal);
+  document.addEventListener('keydown', function(e) {
+    if (e.code === "Escape") {
+      switchModal();
+    }
+  }); 
+  
+  document.addEventListener('click', function(event) {
+    let fromButton = false;
+
+    modalBtn.forEach(element => {
+      if (element.contains(event.target)) {
+        fromButton = true;
+      }
+    });
+
+    if (!fromButton && modal.classList.contains('modal--visible') && !modalDialog.contains(event.target)) {
+      switchModal();
+    }
+  });
 });
