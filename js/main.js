d6c120ed7f52b762c1416057d019bf743513b9a8
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-05-14 19:18:25 +0300

    feat: added jquery features

--- js/main.js
+++ js/main.js
@@ -1,37 +1,63 @@
-//можно проверить на http://www.eugenezaznoba.ru/repair-design
+$(document).ready(function () {
+  const modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__close'),
+        modalDialog = $('.modal__dialog'),
+        scrollUp = $('.scroll-up-container');
 
-document.addEventListener('DOMContentLoaded', function(event) {
-const modal = document.querySelector('.modal');
-const modalDialog = document.querySelector('.modal__dialog');
-const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () => {
+    modal.toggleClass('modal--visible');
+  }
 
-const switchModal = () => {
-  modal.classList.toggle('modal--visible');
-}
+  modalBtn.on('click', switchModal);
+  closeBtn.on('click', switchModal);
 
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-
-  closeBtn.addEventListener('click', switchModal);
-  document.addEventListener('keydown', function(e) {
+  $(document).on('keydown', function(e) {
     if (e.code === "Escape") {
       switchModal();
     }
   }); 
-  
-  document.addEventListener('click', function(event) {
-    let fromButton = false;
 
-    modalBtn.forEach(element => {
-      if (element.contains(event.target)) {
-        fromButton = true;
-      }
-    });
+  $(document).on('click', function(event) {
+    let target = $(event.target);
 
-    if (!fromButton && modal.classList.contains('modal--visible') && !modalDialog.contains(event.target)) {
+    if (!target.is(modalBtn) && modal.hasClass('modal--visible') && !target.closest(modalDialog).length){
       switchModal();
     }
   });
+
+    function scrollFunction() {
+      let scrollTop = $(window).scrollTop();
+
+      if (scrollTop > 20) {
+        scrollUp.css('display', "block");
+      } else {
+        scrollUp.css('display', "none");
+      }
+    }
+    
+    $(window).scroll(scrollFunction);
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
 });
