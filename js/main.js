03a52a94fc269427bbd219bf36b2393d728e65ea
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-05-20 19:22:12 +0300

    refactor

--- js/main.js
+++ js/main.js
@@ -146,33 +146,33 @@ $(document).ready(function () {
       swiper.pagination.dynamicBullets = true;
     }
 
-    //new WOW().init();
-    var $animation_elements = $('.animate');
-    var $window = $(window);
+    new WOW().init();
+    // var $animation_elements = $('.animate');
+    // var $window = $(window);
     
-    function check_if_in_view() {
-      var window_height = $window.height();
-      var window_top_position = $window.scrollTop();
-      var window_bottom_position = (window_top_position + window_height);
+    // function check_if_in_view() {
+    //   var window_height = $window.height();
+    //   var window_top_position = $window.scrollTop();
+    //   var window_bottom_position = (window_top_position + window_height);
      
-      $.each($animation_elements, function() {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = (element_top_position + element_height);
+    //   $.each($animation_elements, function() {
+    //     var $element = $(this);
+    //     var element_height = $element.outerHeight();
+    //     var element_top_position = $element.offset().top;
+    //     var element_bottom_position = (element_top_position + element_height);
      
-        //check to see if this current container is within viewport
-        if ((element_bottom_position >= window_top_position) &&
-            (element_top_position <= window_bottom_position)) {
-          $element.addClass('slidein');
-        } else {
-          $element.removeClass('slidein');
-        }
-      });
-    }
+    //     //check to see if this current container is within viewport
+    //     if ((element_bottom_position >= window_top_position) &&
+    //         (element_top_position <= window_bottom_position)) {
+    //       $element.addClass('slidein');
+    //     } else {
+    //       $element.removeClass('slidein');
+    //     }
+    //   });
+    // }
     
-    $window.on('scroll resize', check_if_in_view);
-    $window.trigger('scroll');
+    // $window.on('scroll resize', check_if_in_view);
+    // $window.trigger('scroll');
 });
 
 
